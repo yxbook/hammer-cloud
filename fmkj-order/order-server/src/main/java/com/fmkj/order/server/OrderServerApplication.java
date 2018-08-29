566d9ac7bb5cf2e09af2a342e4caeeb9949182c2
youxun <13261656101@163.com>
2018-08-29 18:27:17 +0800

    订单服务接口

--- fmkj-order/order-server/src/main/java/com/fmkj/order/server/OrderServerApplication.java
+++ fmkj-order/order-server/src/main/java/com/fmkj/order/server/OrderServerApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @SpringCloudApplication
 @ComponentScan(basePackages = {"com.fmkj.order.server", "com.fmkj.order.dao.domain", "com.fmkj.order.client"})
 @EnableFeignClients/*(basePackages = "com.fmkj.order.client")*/
 @MapperScan("com.fmkj.order.dao.*")
+@EnableSwagger2
 public class OrderServerApplication {
 
 	public static void main(String[] args) {
--- fmkj-order/order-server/src/main/java/com/fmkj/order/server/controller/ProductController.java
+++ fmkj-order/order-server/src/main/java/com/fmkj/order/server/controller/ProductController.java
@@ -8,21 +8,21 @@ import com.fmkj.common.constant.LogConstant;
 import com.fmkj.order.dao.domain.ProductInfo;
 import com.fmkj.order.server.annotation.OrderLog;
 import com.fmkj.order.server.service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 /**
- * 订单服务
+ * 商品服务
  */
 @RestController
 @RequestMapping("/product")
 @DependsOn("springContextHolder")
+@Api(tags ={ "商品服务"},description = "商品服务接口")
 public class ProductController extends BaseController<ProductInfo, ProductService> implements BaseApiService<ProductInfo> {
 
 
@@ -30,10 +30,48 @@ public class ProductController extends BaseController<ProductInfo, ProductServic
     private ProductService productService;
 
 
+    @ApiOperation(value="查询商品列表", notes="分页查询商品列表")
     @OrderLog(module= LogConstant.HC_PRODUCT, actionDesc = "查询商品列表")
     @GetMapping("/getProductPage")
     public BaseResult<Page<ProductInfo>> getProductPage(@RequestParam HashMap<String, Object> params){
-        return super.selectPage(params);
+        try {
+            return super.selectPage(params);
+        } catch (Exception e) {
+           throw new RuntimeException("查询商品列表异常：" + e.getMessage());
+        }
+    }
+
+    @ApiOperation(value="删除商品", notes="根据ID删除商品")
+    @OrderLog(module= LogConstant.HC_PRODUCT, actionDesc = "删除商品")
+    @PostMapping("/deleteById")
+    public BaseResult deleteById(@RequestParam String id){
+        try {
+            return super.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("删除异常：" + e.getMessage());
+        }
+    }
+
+    @ApiOperation(value="新增商品", notes="新增商品")
+    @OrderLog(module= LogConstant.HC_PRODUCT, actionDesc = "新增商品")
+    @PostMapping("/addProduct")
+    public BaseResult addProduct(@RequestParam ProductInfo productInfo){
+        try {
+            return super.insert(productInfo);
+        } catch (Exception e) {
+            throw new RuntimeException("新增异常：" + e.getMessage());
+        }
+    }
+
+    @ApiOperation(value="修改商品", notes="根据ID修改商品")
+    @OrderLog(module= LogConstant.HC_PRODUCT, actionDesc = "修改商品")
+    @PostMapping("/updateProduct")
+    public BaseResult updateProduct(@RequestParam ProductInfo productInfo){
+        try {
+            return super.updateById(productInfo);
+        } catch (Exception e) {
+            throw new RuntimeException("修改异常：" + e.getMessage());
+        }
     }
 
 
