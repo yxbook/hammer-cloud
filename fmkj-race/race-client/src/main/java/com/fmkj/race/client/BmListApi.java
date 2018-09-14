b1fd5f15e3dcb7ec19492fe876c1aaadc94c98f0
youxun <632017924@qq.com>
2018-09-14 15:08:07 +0800

    1、token校验

--- fmkj-race/race-client/src/main/java/com/fmkj/race/client/BmListApi.java
+++ fmkj-race/race-client/src/main/java/com/fmkj/race/client/BmListApi.java
@@ -3,21 +3,18 @@ package com.fmkj.race.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-
-import java.util.HashMap;
 
 @FeignClient(name="FMKJ-USER", fallback = BmListApi.HcTokenClientFallBack.class)
 public interface BmListApi {
 
     @PostMapping("/bmList/isBlacker")
-    Boolean isActivityBlack(HashMap<String, Object> params);
+    Boolean isActivityBlack(Integer id, Integer status);
 
     @Component
     public static class HcTokenClientFallBack implements BmListApi {
 
         @Override
-        public Boolean isActivityBlack(HashMap<String, Object> params) {
+        public Boolean isActivityBlack(Integer id, Integer status) {
             return null;
         }
     }
